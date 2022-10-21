90b8b62236195e842e656e2f01a58c303a2c6455
AndrePcastelo <andreperezcastelo@protonmail.ch>
2022-10-21 15:08:11 -0300

    Update--01, calculador de Fatorial

--- python-software/Calculador de fatorial.py
+++ python-software/Calculador de fatorial.py
@@ -1,11 +1,22 @@
 #Calculadora de Fatorial Simples
 import numpy
 
+c = 's'
+res = 's'
 lista = []
+lista2 = []
 a = int(input('Digite um número:'))
-print(f'A fatorial deste {a}! número é: ')
+res = str(input('Você quer adicionar um número fatorial repetido? [s/n]'))
+if res == 's':
+    while c == 's':
+     lista2.append(int(input('Digite o número repetido:')))
+     c = str(input('Voce quer adicionar mais um número? [s/n]'))
 for a in range(a, 0, -1):
     print(f'{a}.')
     lista.append(a)
+for lista2 in range (lista2[0], 0, -1):
+    print(f'{lista2}.')
 print(lista)
 print(numpy.prod(lista))
+print(lista2)
+
